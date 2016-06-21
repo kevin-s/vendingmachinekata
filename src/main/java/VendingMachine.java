8b0e1a832e3e9c57c11b48f339d4427480c02f8b
Kevin <kevin-s@users.noreply.github.com>
2016-06-20 21:01:36 -0500

    first class and first passing test

---
+++ src/main/java/VendingMachine.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kevin
+ * Date: 6/20/16
+ * Time: 8:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VendingMachine {
+
+    public VendingMachine() {
+
+    }
+
+
+
+}
---
+++ src/test/java/VendingMachineTest.java
@@ -0,0 +1,22 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kevin
+ * Date: 6/20/16
+ * Time: 8:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class VendingMachineTest {
+
+    @Test
+    public void testMachineStartup() {
+
+        VendingMachine machine = new VendingMachine();
+        assertTrue(true);
+    }
+}
